0b7391a320eae9ebbdcacb0e60d7508cfd3addf8
alexurve <alexurve@alexurve-X399-AORUS-PRO>
2020-10-20 17:06:58 +0200

    H

--- Azamon/src/IA/Azamon/Estado.java
+++ Azamon/src/IA/Azamon/Estado.java
@@ -7,11 +7,11 @@ public class Estado {
 	private static Transporte t; // vector de ofertes ( posem static perque tots els estats tindran el mateix vector t)
 	private double preu; // total del que costara el estat 
 	private int felicitat; //felicitat del estat
-	private ArrayList<Integer> assig; // relació entre paquets i ofertes -> per assig[i] la i correspon al identificador del p
+	private ArrayList<Integer> assig; // relaciï¿½ entre paquets i ofertes -> per assig[i] la i correspon al identificador del p
 									  //paquet i la assig[i] es la oferta -> si la assig[i] = -1 significa que el paquet no esta assignat a cap oferta
 	private ArrayList<Double> pes_oferta; // pes actual que te la oferta -> la i es la oferta i pes_oferta[i] es el pes d'aquesta oferta
 	
-	
+	d
 	
 
 }
